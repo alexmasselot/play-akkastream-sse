2c50990396c1464cbd1df98595926382fb0a5ecf
Alexandre Masselot <amasselot@octo.com>
2016-11-14 08:18:42 +0100

    with solution

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 server sent events with Play
 ============================
 
+This project was written to sustain a akka-user post (https://groups.google.com/forum/#!topic/akka-user/tUEqBwgrJYQ) 
+
 Launch the code:
 
     ./activator ~run
 
-This project expose 2 type of ways to generate SSE:
+This project expose 3 ways to generate SSE:
 
  * **WORKS** `http://locahost:9000/ticks` shoots texts via `Source.tick`
- * **FAILS** `http://localhost:9000/actor-flow` shoots two messages via a akkastream Source.actorRef
+ * **FAILS** `http://localhost:9000/actor-flow` does not shoot two messages via a akkastream Source.actorRef
+ * **SOLUTION** `http://localhost:9000/actor-flow-ok` shoots two messages via a akkastream Source.actorRef
 
 
 Check with curl:
@@ -24,7 +27,17 @@ Check with curl:
     
     octomaa:tmp alex$  curl  -H "Accept: text/event-stream" http://localhost:9000/actor-flow
     ^C    
+    
+    octomaa:tmp alex$  curl  -H "Accept: text/event-stream" http://localhost:9000/actor-flow-ok
+    data: PAF
+    
+    data: delayed PIF
+    
+    ^C
 
 **Problem** : The second method sees the messages going through the akkastream flow but are not sent through the http channel...
 
- All the code is in `app/controllers/StreamController`
+**Solution**: The problem was having two flow and exposing the wrong materialization (thanks Viktor).
+It is solved in the actorFlowWorking action.
+
+All the code is in `app/controllers/StreamController`
--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -1,16 +1,13 @@
 package controllers
 
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import javax.inject.{Inject, Singleton}
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.ActorMaterializer
 import akka.stream.OverflowStrategy._
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
-import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext
@@ -35,23 +32,21 @@ class StreamController @Inject()()(implicit exec: ExecutionContext, actorSystem:
   }
 
   def actorFlow = Action {
-    val source = Source.actorRef[String](10, fail)
-      .map({ (x) =>
-        //the message go through this stage
-        println(s"through source '$x'")
-        x
-      })
+    val (ref, publisher) = Source.actorRef[String](10, fail).toMat(Sink.asPublisher(true))(Keep.both).run()
 
-    val ref = Flow[String]
-      .to(Sink.ignore)
-      .runWith(
-        source
-      )
-
-    //send messages. One is now, oth is delayed
     ref ! "PAF"
     actorSystem.scheduler.scheduleOnce(1000 milliseconds, ref, "delayed PIF")
 
-    Ok.chunked(source via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+
+    Ok.chunked(Source.fromPublisher(publisher) via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+  }
+
+  def actorFlowWorking = Action {
+    val (ref, publisher) = Source.actorRef[String](10, fail).toMat(Sink.asPublisher(true))(Keep.both).run()
+
+    ref ! "PAF"
+    actorSystem.scheduler.scheduleOnce(1000 milliseconds, ref, "delayed PIF")
+
+    Ok.chunked(Source.fromPublisher(publisher) via EventSource.flow).as(ContentTypes.EVENT_STREAM)
   }
 }
